4814b39a9ffaa99b4cc392e3243e940ebc4b9859
magnusjacobsen <magnusjacobsen@gmx.com>
2021-02-28 12:31:30 +0100

    backup

--- inspect_model.py
+++ inspect_model.py
@@ -20,25 +20,64 @@ def dtype_bits(param):
         return 8
     raise TypeError(f'Could not recognize dtype: {dt}') 
 
-total_weights = 0
-total_params = 0
-total_bits = 0
-total_mbs = 0
-for name, param in roberta.named_parameters():
-    size = list(param.size())
-    num_weights = reduce(lambda acc, x: acc * x, size, 1)
-    num_bits = num_weights * dtype_bits(param)
+def group_by_layer(model):
+    layers = {}
+    for name, param in roberta.named_parameters():
+        if 'classification_heads' in name:
+            key = name.split('classification_heads.')[1].split('.')[0]
+        elif 'layers.' in name:
+            key = 'layer_' + (name.split('layers.')[1].split('.')[0])
+        else:
+            key = 'layer_encoding'
+        values = layers.get(key, [])
+        values.append((name,param))
+        layers[key] = values
+    return layers
 
-    total_weights += num_weights
-    total_bits += num_bits
-    total_mbs += num_bits / 8_000_000
-    total_params += 1
-    print(name, list(param.size()))
+def print_parameter_size(model):
+    total_weights = 0
+    total_params = 0
+    total_bits = 0
+    total_mbs = 0
+    for name, param in model.named_parameters():
+        size = list(param.size())
+        num_weights = reduce(lambda acc, x: acc * x, size, 1)
+        num_bits = num_weights * dtype_bits(param)
 
-print('******************')
-print(f'Number of weights: {total_weights}')
-print(f'Number of named params: {total_params}')
-print('---------------')
-print('Size of weights:')
-print(f'{total_bits} bits')
-print(f'{total_mbs:.2f} MB')
+        total_weights += num_weights
+        total_bits += num_bits
+        total_mbs += num_bits / 8_000_000
+        total_params += 1
+
+    print(f'Number of weights: {total_weights}')
+    print(f'Number of named params: {total_params}')
+    print('---------------')
+    print('Size of weights:')
+    print(f'{total_bits} bits')
+    print(f'{total_mbs:.2f} MB')
+
+def ratio_below_treshold(model, threshold):
+    total_weights = 0
+    below = 0
+    for name, param in model.named_parameters():
+        if '.weight' in name:
+            print(name)
+            for w in param.view(-1):
+                total_weights += 1
+                if abs(w) < threshold:
+                    below += 1
+                    print('below')
+                else:
+                    print('above')
+        else:
+            print(f'not applicable for: {name}')
+    print(f'total weights: {total_weights}')
+    print(f'below threshold of {threshold}: {below}')
+    print(f'ratio: {below / total_weights}')
+
+#layers = group_by_layer(roberta)
+#ratio_below_treshold(roberta, 0.0001)
+print() .data?
+
+print('-'*20)
+print_parameter_size(roberta)
