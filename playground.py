8ab02204fc84949d701a25db8e89b4121e1d190a
magnusjacobsen <magnusjacobsen@gmx.com>
2021-03-12 11:22:58 +0100

    removing playground file

--- playground.py
+++
@@ -1,6 +0,0 @@
-data_path = 'data/glue/MNLI/dev_matched.tsv'
-with open(data_path) as fin:
-    fin.readline()
-    line = fin.readline().strip().split('\t')
-    for segment in line:
-        print(segment)
