3cf1141ad91b2a724c359b1b9e5a869039fdf1ff
Mikkel Sørensen <m_hooge@hotmail.com>
2021-03-25 14:04:43 +0100

    Fixed a thing or two.

--- compression/distillation/student_models/bpe_ffn.py
+++ compression/distillation/student_models/bpe_ffn.py
@@ -2,7 +2,7 @@ from numpy.core.fromnumeric import squeeze
 import torch
 import torch.nn as nn
 from bpemb import BPEmb
-from common.task_utils import TASK_INFO, TASK_LABEL_DICT
+from common.task_utils import TASK_LABEL_DICT
 from compression.distillation.student_models.glue_bilstm import StudentConfig
 
 class BPE_FFN(nn.Module):
@@ -13,7 +13,8 @@ class BPE_FFN(nn.Module):
             task,
             use_gpu,
             embedding_dim=25,
-            vocab_size=5000
+            vocab_size=10000,
+            dropout=0.3
         )
 
         self.max_seq_len = 150
@@ -25,19 +26,32 @@ class BPE_FFN(nn.Module):
         )
         self.embedding = nn.Embedding.from_pretrained(torch.tensor(self.bpe.vectors))
 
+        self.dropout = nn.Dropout(p=self.cfg.dropout) if self.cfg.dropout else lambda x: x
+
         self.avg_pool = nn.AvgPool1d(2)
 
-        self.hidden_units = 500
+        self.hidden_units = 256
 
         self.classifier = nn.Sequential(
-            nn.Linear(self.max_seq_len * self.cfg.embedding_dim // 2, self.hidden_units),
-            nn.Linear(self.hidden_units, self.cfg.num_classes)
+            nn.Linear(self.max_seq_len * (self.cfg.embedding_dim // 2), self.hidden_units),
+            nn.Dropout(p=self.cfg.dropout),
+            nn.Linear(self.hidden_units, self.hidden_units),
+            nn.Dropout(self.cfg.dropout),
+            nn.Linear(self.hidden_units, self.cfg.num_classes),
+            nn.ReLU()
         )
 
-    def forward(self, sents, _):
+    def forward(self, sents, lens):
         emb = self.embedding(sents).float()
 
-        x = self.avg_pool(emb)
+        pad_amount = self.max_seq_len - emb.shape[1]
+
+        # Pad 2nd dimension to match max_seq_len.
+        emb = torch.nn.functional.pad(emb, pad=(0, 0, 0, pad_amount, 0, 0), mode="constant")
+
+        x = self.dropout(emb)
+
+        x = self.avg_pool(x)
 
         x = x.view(self.cfg.batch_size, -1)
 
--- distill.py
+++ distill.py
@@ -10,61 +10,8 @@ from compression.distillation.models import (
     TangBILSTM,
     GlueBILSTM,
     BPE_FFN,
-    DistLossFunction,
-    LabelLossFunction
+    DistLossFunction
 )
-
-def main(args, sacred_experiment=None):
-    print("Sit back, tighten your seat belt, and prepare for the ride of your life 🚀")
-
-    device = torch.device('cpu') if args.cpu else torch.device('cuda')
-    use_gpu = not args.use_cpu
-    
-    if args.generate_loss:
-        data.generate_distillation_loss(args)
-    if args.augment:
-        data_augment.augment(args.task, args.augment)
-    if args.play:
-        torch.manual_seed(233)
-        task = args.task
-        student_type = args.student_arch
-
-        if student_type == 'glue':
-            model = GlueBILSTM(task, use_gpu)
-        elif student_type == 'tang':
-            model = TangBILSTM(task, use_gpu)
-        elif student_type == 'wasserblat-ffn':
-            model = BPE_FFN(task, use_gpu)
-
-        if args.size:
-            total_params, total_bits = parameters.get_model_size(model)
-            print(type(model))
-            print(f'total params: {total_params / 1000}K)')
-            print(f'total size:   {total_bits / 8000000:.2f}MB')
-
-        base_path = f'{TASK_INFO[task]["path"]}/distillation_data'
-        distillation_data = []
-        train_files = glob(f"{base_path}/*.tsv")
-        for filename in train_files:
-            distill_data = data.load_distillation_data(filename)
-
-            if distillation_data == []:
-                distillation_data = distill_data
-            else:
-                distillation_data[0].extend(distill_data[0])
-                distillation_data[1].extend(distill_data[1])
-                distillation_data[2].extend(distill_data[2])
-                if len(distillation_data) > 3:
-                    distillation_data[3].extend(distill_data[3])
-
-        val_data = data.load_val_data(task)
-        model.to(device)
-
-        criterion = DistLossFunction(0.5, nn.MSELoss(), nn.CrossEntropyLoss(), device)
-        dataloaders = data.get_dataloader_dict(model, distillation_data, val_data)
-        #optim = torch.optim.Adadelta(model.parameters())
-        optim = torch.optim.Adam(model.parameters(), lr=1e-4)
-        train_loop(model, criterion, optim, dataloaders, device)
 
 # only works for single sentence prediction
 def train_loop(model, criterion, optim, dl, device, num_epochs=10):
@@ -107,6 +54,62 @@ def train_loop(model, criterion, optim, dl, device, num_epochs=10):
             num_examples += len(lens)
         print(f'|--> val accuracy: {running_corrects / num_examples:.4f}')
 
+def main(args, sacred_experiment=None):
+    print("Sit back, tighten your seat belt, and prepare for the ride of your life 🚀")
+
+    device = torch.device('cpu') if args.cpu else torch.device('cuda')
+    use_gpu = not args.cpu
+    epochs = args.epochs
+    
+    if args.generate_loss:
+        data.generate_distillation_loss(args)
+    elif args.augment:
+        data_augment.augment(args.task, args.augment)
+    elif args.play:
+        torch.manual_seed(233)
+        task = args.task
+        student_type = args.student_arch
+
+        if student_type == 'glue':
+            model = GlueBILSTM(task, use_gpu)
+        elif student_type == 'tang':
+            model = TangBILSTM(task, use_gpu)
+        elif student_type == 'wasserblat-ffn':
+            model = BPE_FFN(task, use_gpu)
+
+        if args.size:
+            total_params, total_bits = parameters.get_model_size(model)
+            print(type(model))
+            print(f'total params: {total_params / 1000}K)')
+            print(f'total size:   {total_bits / 8000000:.2f}MB')
+
+        base_path = f'{TASK_INFO[task]["path"]}/distillation_data'
+        distillation_data = []
+        train_files = glob(f"{base_path}/*.tsv")
+        for filename in train_files:
+            distill_data = data.load_distillation_data(filename)
+
+            if distillation_data == []:
+                distillation_data = distill_data
+            else:
+                distillation_data[0].extend(distill_data[0])
+                distillation_data[1].extend(distill_data[1])
+                distillation_data[2].extend(distill_data[2])
+                if len(distillation_data) > 3:
+                    distillation_data[3].extend(distill_data[3])
+
+        print(f"*** Loaded {len(distillation_data[0])} training data samples ***")
+
+        val_data = data.load_val_data(task)
+        model.to(device)
+
+        print(f"*** Loaded {len(val_data[0])} validation data samples ***")
+
+        criterion = DistLossFunction(0.5, nn.MSELoss(), nn.CrossEntropyLoss(), device)
+        dataloaders = data.get_dataloader_dict(model, distillation_data, val_data)
+        #optim = torch.optim.Adadelta(model.parameters())
+        optim = torch.optim.Adam(model.parameters(), lr=1e-4)
+        train_loop(model, criterion, optim, dataloaders, device, epochs)
 
 if __name__ == "__main__":
     ARGS = argparsers.args_distill()
