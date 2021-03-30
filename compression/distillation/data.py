9a663a439e572e6a603c8cbc0456d1cad53544e7
Mikkel Sørensen <m_hooge@hotmail.com>
2021-03-30 13:47:40 +0200

    Generate loss data now actually works for augmented data.

--- compression/distillation/data.py
+++ compression/distillation/data.py
@@ -7,9 +7,9 @@ import os
 from glob import glob
 
 # returns sentences, labels
-def load_train_data(task, ds_type='train', data_folder="processed"):
+def load_train_data(task, ds_type='train'):
     files = [ds_type + x for x in ['.raw.input0', '.raw.input1', '.label']]
-    folder = f'{TASK_INFO[task]["path"]}/{data_folder}/'
+    folder = f'{TASK_INFO[task]["path"]}/processed/'
 
     with open(folder + files[0], encoding='utf-8') as sentences:
         with open(folder + files[2], encoding='utf-8') as targets:
@@ -18,6 +18,17 @@ def load_train_data(task, ds_type='train', data_folder="processed"):
                     return sentences.readlines(), targets.readlines(), sentences2.readlines()
             except FileNotFoundError:
                 return sentences.readlines(), targets.readlines()
+
+def load_augment_data(task, augment_type):
+    files = [f'{augment_type}.input0', f'{augment_type}.input1']
+    folder = f'{TASK_INFO[task]["path"]}/augment_data/'
+
+    with open(folder + files[0], encoding='utf-8') as sentences:
+        try:
+            with open(folder + files[1], encoding="utf-8") as sentences2:
+                return sentences.readlines(), sentences2.readlines()
+        except FileNotFoundError:
+            return (sentences.readlines(),)
 
 def load_all_distillation_data(task):
     base_path = f'{TASK_INFO[task]["path"]}/distillation_data'
@@ -38,25 +49,35 @@ def load_all_distillation_data(task):
 
 def generate_for_train_data(model, args):
     input_folder = args.generate_loss
-    data = load_train_data(args.task, data_folder=input_folder)
+    # We are working with regular pre-labelled data (not augmented data).
+    augmented_data = input_folder != "processed"
+    if augmented_data: # Load only sentences for augmented data.
+        data = load_augment_data(args.task, input_folder)
+        sentence_pairs = len(data) == 2
+    else: # Load sentences + target label.
+        data = load_train_data(args.task)
+        sentence_pairs = len(data) == 3
+
     batch_size = 8
     n = len(data[0])
-    sentence_pairs = len(data) == 3
     output_path = f'{TASK_INFO[args.task]["path"]}/distillation_data/'
 
     if not os.path.exists(output_path):
         os.mkdir(output_path)
 
-    output_file = "train.tsv" if input_folder == "processed" else input_folder
+    output_file = "train.tsv" if input_folder == "processed" else input_folder + ".tsv"
+    label_fn = lambda label: model.task.label_dictionary.string([label + model.task.label_dictionary.nspecial])
 
     with open(output_path + output_file, 'w', encoding='utf-8') as out:
         for i in range(int((n - 1) / batch_size) + 1):
             start = i * batch_size
             end = start + batch_size if (start + batch_size) < n else n
             batch_sents = data[0][start : end]
-            batch_targets = data[1][start : end]
+            if not augmented_data: # Use target labels for regular labeled data.
+                batch_targets = data[1][start : end]
+
             if sentence_pairs:
-                batch_sents2 = data[2][start : end]
+                batch_sents2 = data[-1][start : end]
                 batch = collate_tokens(
                     [model.encode(sent1, sent2) for sent1, sent2 in zip(batch_sents, batch_sents2)],
                     pad_idx=1
@@ -67,6 +88,8 @@ def generate_for_train_data(model, args):
                     pad_idx=1
                 )
             batch_logits = model.predict('sentence_classification_head', batch, return_logits=True)
+            if augmented_data: # Use predicted labels from logits for augmented data.
+                batch_targets = [label_fn(label) for label in batch_logits.argmax(dim=1).tolist()]
 
             if sentence_pairs:
                 for sent1, sent2, target, logits in zip(batch_sents, batch_sents2, batch_targets, batch_logits.tolist()):
