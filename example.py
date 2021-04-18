32263d1da7d0e783885e9e1a07bd125c45e7d144
magnusjacobsen <magnusjacobsen@gmx.com>
2021-04-18 21:41:40 +0200

    whoop whoop bug begone

--- example.py
+++ example.py
@@ -1,13 +1,35 @@
 import compression.distillation.student_models.char_rnn as c
 import torch
+import torch.nn as nn
+import torch.quantization as quant
 
-emb = c.HashEmbedding(3, 10)
+class Model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.emb = nn.EmbeddingBag(2,10)
+        self.qemb = nn.quantized.EmbeddingBag(2, 10)
 
-sent = "hej med dig"
-input = torch.stack([emb.encode(sent)])
-print(input)
-print(input.shape)
-out = emb(input)
+    def forward(self, x, use_quantized):
+        if use_quantized:
+            return self.qemb(x)
+        return self.emb(x)
+backend = 'fbgemm'
+torch.backends.quantized.engine = backend
+#torch.manual_seed(1)
+model = Model()
+#model.qemb.qconfig = quant.float_qparams_weight_only_qconfig
+#model.qconfig = quant.default_qconfig
+#quant.prepare(model.qemb, inplace=True)
 
-print(out)
-print(out.shape)
+input1 = torch.stack([torch.LongTensor([0])])
+input2 = torch.stack([torch.LongTensor([0,1])])
+out1 = model(input1, False)
+print(out1, out1.dtype)
+qout1 = model(input1, True)
+print(qout1, qout1.dtype)
+out2 = model(input2, False)
+print(out2, out2.dtype)
+qout2 = model(input2, True)
+print(qout2, qout2.dtype)
+
+#quant.convert(model.qemb, inplace=True)
