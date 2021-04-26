5979df3020b70934251a76ae234b6265ec5f7029
Mikkel Sørensen <m_hooge@hotmail.com>
2021-04-26 13:52:49 +0200

    Now it totally works!

--- preprocess.py
+++ preprocess.py
@@ -13,7 +13,7 @@ def main(args):
     elif args.augment is not None:
         data_augment.augment(args.task, args.augment, args.seed)
     elif args.generate_loss is not None:
-        teacher_model = model_utils.load_teacher(args.task, f"models/finetuned/{args.checkpoint_path}", args.cpu)
+        teacher_model = model_utils.load_teacher(args.task, f"models/finetuned/{args.task}", args.cpu, args.checkpoint_path)
         distillation_loss.generate_distillation_loss(args, teacher_model)
 
 if __name__ == "__main__":
