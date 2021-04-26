9dca12072c2208ad0604f5cf307318cff95d2ecd
Mikkel Sørensen <m_hooge@hotmail.com>
2021-04-26 13:30:05 +0200

    Fixed distillation loss missing argument.

--- preprocess.py
+++ preprocess.py
@@ -13,8 +13,7 @@ def main(args):
     elif args.augment is not None:
         data_augment.augment(args.task, args.augment, args.seed)
     elif args.generate_loss is not None:
-        # TODO: fix checkpoint path thingie. Where do we store finetuned models in the current code?
-        teacher_model = model_utils.load_teacher(args.task, args.checkpoint_path, args.cpu)
+        teacher_model = model_utils.load_teacher(args.task, f"models/finetuned/{args.checkpoint_path}", args.cpu)
         distillation_loss.generate_distillation_loss(args, teacher_model)
 
 if __name__ == "__main__":
