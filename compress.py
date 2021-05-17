0f2fe4681ce3abea2dd3dbe7641306448bcb7d40
Mikkel Sørensen <m_hooge@hotmail.com>
2021-05-17 22:30:24 +0200

    Or not.

--- compress.py
+++ compress.py
@@ -4,8 +4,14 @@ import torch.nn as nn
 from argparse import ArgumentError
 import json
 import os
+<<<<<<< HEAD
+
+import torch
+import torch.nn as nn
+=======
 import warnings
 import random
+>>>>>>> f653c2e6b5a4d56c52cae3d7b6b66bf8d0d89dcb
 import numpy as np
 
 from common import argparsers, data_utils, transponder
@@ -176,10 +182,13 @@ def main(args, sacred_experiment=None):
     seed = args.seed
 
     random.seed(seed)
+    np.random.seed(seed)
     torch.manual_seed(seed)
     np.random.seed(seed)
     if torch.cuda.is_available():
         torch.cuda.manual_seed(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
 
     should_prune = "prune" in args.compression_actions
     should_quantize = "quantize" in args.compression_actions
