1ccf4c9a776378351f9e9617a21f5dfa08775a0d
Mikkel Sørensen <m_hooge@hotmail.com>
2021-05-17 22:26:46 +0200

    Revert :(

--- compress.py
+++ compress.py
@@ -176,14 +176,10 @@ def main(args, sacred_experiment=None):
     seed = args.seed
 
     random.seed(seed)
-    np.random.seed(seed)
     torch.manual_seed(seed)
     np.random.seed(seed)
-    torch.use_deterministic_algorithms(True)
     if torch.cuda.is_available():
         torch.cuda.manual_seed(seed)
-        torch.backends.cudnn.deterministic = True
-        torch.backends.cudnn.benchmark = False
 
     should_prune = "prune" in args.compression_actions
     should_quantize = "quantize" in args.compression_actions
