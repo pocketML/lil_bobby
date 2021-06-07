dafc449b0fc572ab1796208b17daa32d40762d1a
Mikkel Sørensen <m_hooge@hotmail.com>
2021-06-07 12:38:12 +0200

    Latex table generatio now chooses only newest results.

--- latex_tables.py
+++ latex_tables.py
@@ -21,15 +21,13 @@ def get_results():
         for day in range(start_day_in_month[month_index], end_day_in_month[month_index] + 1):
             results_for_day = glob(f"experiments/*_{month}{day}*")
             results_for_day.sort(key=get_experiment_suffix)
-            if results_for_day > 4:
-                results_for_day = results_for_day[-4:]
 
             grouped_results = {}
             for result in results_for_day:
                 split = result.split("_")
 
                 name = "_".join(split[:-1])
-                if name not in grouped_results:
+                if name not in grouped_results or len(grouped_results[name]) == 4:
                     grouped_results[name] = []
                 grouped_results[name].append(result)
 
