e1240f5ef898bd0d168af57339a97baa52b90d64
Mikkel Sørensen <m_hooge@hotmail.com>
2021-06-27 18:51:16 +0200

    Average word occurence of predicted sentences!

--- task_difficulty.py
+++ task_difficulty.py
@@ -13,6 +13,10 @@ MODEL_NAMES = {
 }
 
 def get_wrong_predictions_roberta(model, val_data, task, device):
+    """
+    Return all indices of sentences in the validation dataset
+    that a finetuned roberta model predicted wrongly.
+    """
     label_fn = lambda label: model.task.label_dictionary.string([label + model.task.label_dictionary.nspecial])
     wrong_predictions = []
     index = 0
@@ -33,6 +37,10 @@ def get_wrong_predictions_roberta(model, val_data, task, device):
     return wrong_predictions
 
 def get_wrong_predictions_distilled(model, val_data, task, device):
+    """
+    Return all indices of sentences in the validation dataset
+    that a distilled model predicted wrongly.
+    """
     wrong_predictions = []
     batch_offset = 0
     for x, lens, target_labels, _ in val_data:
@@ -109,36 +117,113 @@ def load_data(model, task, is_roberta_model):
 
     return data_utils.get_val_dataloader(model, val_data, shuffle=False)
 
-def analyze_sentence_lengths(wrong_sentences, all_sentences):
+def analyze_sentence_lengths(task, wrong_sentences, all_sentences):
+    """
+    Return the average length of sentences that each model
+    classified correctly and incorrectly, respectively.
+    """
     avg_lengths = {}
-    
+
+    total_sentences = sum(len(sent_list) for sent_list in all_sentences)
+
     for arch in wrong_sentences:
-        wrong_set = set(x[0] for x in wrong_sentences[arch])
+        wrong_set = set()
+        # Create a set of sentences that a model got wrong.
+        for sent_data in wrong_sentences[arch]:
+            if task_utils.is_sentence_pair(task):
+                wrong_set.update(sent_data[0])
+                wrong_set.update(sent_data[1])
+            else:
+                wrong_set.update(sent_data)
+
         sum_len_wrong = 0
         sum_len_correct = 0
-        for sentence in all_sentences:
-            if sentence not in wrong_set:
-                sum_len_correct += len(sentence)
-            else:
-                sum_len_wrong += len(sentence)
+
+        # Go through all sentences in the validation dataset.
+        # Sum lengths of sentences that were correctly and incorrectly classified.
+        for sent_list in all_sentences:
+            for sentence in sent_list:
+                if sentence not in wrong_set:
+                    sum_len_correct += len(sentence)
+                else:
+                    sum_len_wrong += len(sentence)
+
+        # Calculate total amount of correct/incorrect sentences.
+        total_sentences_right = total_sentences - len(wrong_set)
+        total_sentences_wrong = len(wrong_set)
+        if task_utils.is_sentence_pair(task):
+            total_sentences_right *= 2
+            total_sentences_wrong *= 2
+
         avg_lengths[arch] = (
-            sum_len_correct / (len(all_sentences) - len(wrong_set)),
-            sum_len_wrong / len(wrong_set)
+            sum_len_correct / total_sentences_right,
+            sum_len_wrong / total_sentences_wrong
         )
     return avg_lengths
 
-def analyze_rare_words(task, wrong_sentences, all_sentences):
+def analyze_rare_words(task, wrong_sentences, all_val_sentences):
+    """
+    Return the average occurence of words in the training set
+    in validation sentences that each model classified
+    correctly and incorrectly, respectively.
+    """
     counts = {}
-    for sentence in all_sentences:
-        for word in sentence:
-            counts[word] = counts.get(word, 0) + 1
+    folder = f'{task_utils.TASK_INFO[task]["path"]}/processed/'
+    files = ['train.raw.input0']
+    if task_utils.is_sentence_pair(task):
+        files.append('train.raw.input1')
 
-    sum_occurence_correct = 0
-    sum_occurence_wrong = 0
+    # Count how often every word in the training set occured.
+    for train_file in files:
+        with open(folder + train_file, encoding='utf-8') as fp:
+            for sentence in fp:
+                for word in sentence:
+                    counts[word] = counts.get(word, 0) + 1
 
+    avg_occurences = {}
+
+    for arch in wrong_sentences:
+        wrong_set = set()
+        # Create a set of sentences that a model got wrong.
+        for sent_data in wrong_sentences[arch]:
+            if task_utils.is_sentence_pair(task):
+                wrong_set.update(sent_data[0])
+                wrong_set.update(sent_data[1])
+            else:
+                wrong_set.update(sent_data)
+
+        words_in_correct_sent = set()
+        words_in_wrong_sent = set()
+
+        # Create a set consisting of all words in sentences that
+        # were correctly/incorrectly classified.
+        for sent_list in all_val_sentences:
+            for sentence in sent_list:
+                for word in sentence.split(" "):
+                    if sentence in wrong_set:
+                        words_in_correct_sent.add(word)
+                    else:
+                        words_in_wrong_sent.add(word)
+
+        sum_occurence_correct = 0
+        sum_occurence_wrong = 0
+
+        # Sum up how often each word in correct/incorrect sentences
+        # occured in the training set.
+        for word in words_in_correct_sent:
+            sum_occurence_correct += counts.get(word, 0)
+        for word in words_in_wrong_sent:
+            sum_occurence_wrong += counts.get(word, 0)
+
+        avg_occurences[arch] = (
+            sum_occurence_correct / len(words_in_correct_sent),
+            sum_occurence_wrong / len(words_in_wrong_sent)
+        )
+    return avg_occurences
 
 def analyze_answers(task, sentences, labels):
     indices_for_models = []
+    # For each model, load indexes of the sentences that the model classified incorrectly.
     for arch in MODEL_NAMES:
         with open(f"misc/wrong_answers_{task}_{arch}.txt", "r", encoding="utf-8") as fp:
             indices_for_models.append([int(x.strip()) for x in fp])
@@ -147,18 +232,31 @@ def analyze_answers(task, sentences, labels):
 
     sentences_for_models = {}
 
+    # Load validation sentences and labels for the sentences that each model got wrong.
     for arch, model_indices in zip(MODEL_NAMES, indices_for_models):
         model_sentences = []
         for index in model_indices:
-            model_sentences.append((sentences[index], labels[index]))
+            sents = sentences[0][index]
+            if len(sentences) == 2:
+                sent_2 = sentences[1][index]
+                sents = (sents, sent_2)
+
+            model_sentences.append((sents, labels[index]))
         sentences_for_models[arch] = model_sentences
 
-    sentence_lengths = analyze_sentence_lengths(sentences_for_models, sentences)
-    
+    sentence_lengths = analyze_sentence_lengths(task, sentences_for_models, sentences)
+
     for arch in sentence_lengths:
         print(f"---===--- {arch} ---===---")
         print(f"Avg. len. correct sents: {sentence_lengths[arch][0]}")
         print(f"Avg. len. wrong sents:   {sentence_lengths[arch][1]}")
+
+    rare_words = analyze_rare_words(task, sentences_for_models, sentences)
+
+    for arch in rare_words:
+        print(f"---===--- {arch} ---===---")
+        print(f"Avg. occurence of words correct sents: {rare_words[arch][0]}")
+        print(f"Avg. occurence of words wrong sents:   {rare_words[arch][1]}")
 
     # shared_wrong_answers = []
     # for count in range(1, len(sentences_for_models)):
@@ -182,17 +280,6 @@ def analyze_answers(task, sentences, labels):
     # print(f"Shared across FFN + RNN: {len(shared_wrong_answers[2])}")
 
 def main(args):
-    # Idea:
-    # 1. Load all data for a task.
-    # 2. Load every trained model (even finetuned). Start with worst model (FFN).
-    # 3. For every model, run through every data point, add all wrong predictions to a list.
-    # 4. See which questions all the 'wrong lists' have in common. 
-    # 5. What are characteristics of these questions? Why are they hard?
-    # 5. Look at lengths of sentences they get wrong vs. right.
-    # 5. Look at whether there are rare/out of vocab words in sentences they get wrong.
-    # 5. Look at whether specific words decrease accuracy in a meaningful way.
-    # 6. ???
-    # 7. Profit!
     all_exists = True
     for arch in MODEL_NAMES:
         if not os.path.exists(f"misc/wrong_answers_{args.task}_{arch}.txt"):
@@ -202,8 +289,14 @@ def main(args):
     if not all_exists:
         calculate_wrong_predictions(args)
 
-    sentences, labels = data_utils.load_train_data(args.task, ds_type="dev")
-    sentences = list(reversed([x.strip() for x in sentences]))
+    val_data = data_utils.load_train_data(args.task, ds_type="dev")
+    if task_utils.is_sentence_pair(args.task):
+        sents_1, labels, sents_2 = val_data
+        sentences = [sents_1, sents_2]
+    else:
+        sents_1, labels = val_data
+        sentences = [sents_1]
+    sentences = [list(reversed([x.strip() for x in sents])) for sents in sentences]
     labels = list(reversed([x.strip() for x in labels]))
 
     analyze_answers(args.task, sentences, labels)
