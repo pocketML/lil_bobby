d689dfcd5983f2a0b356f2fd00561eef5832db7c
Mikkel Sørensen <m_hooge@hotmail.com>
2021-07-08 23:05:15 +0200

    I despise Sacred with my entire being.

---
+++ misc/fix_sacred.py
@@ -0,0 +1,32 @@
+import argparse
+import json
+
+def main(args):
+    metrics_path = f"experiments/{args.name}/metrics.json"
+    with open(metrics_path) as fp:
+        metrics_data = json.load(fp)
+        keys = [
+            args.params, args.nonzero_params, args.model_disk_size,
+            args.theoretical_size, args.eval_acc
+        ]
+        for key in keys:
+            if key is not None:
+                print(f"Setting {key} to {metrics_data[key]}")
+                metrics_data[key] == getattr(args, key)
+
+    with open(metrics_path, "w", encoding="utf-8") as fp:
+        json.dump(metrics_data, fp)
+
+if __name__ == "__main__":
+    AP = argparse.ArgumentParser()
+
+    AP.add_argument("--name", type=str, required=True)
+    AP.add_argument("--params")
+    AP.add_argument("--nonzero-params")
+    AP.add_argument("--model-disk-size")
+    AP.add_argument("--theoretical-size")
+    AP.add_argument("--eval-acc")
+
+    ARGS = AP.parse_args()
+
+    main(ARGS)
