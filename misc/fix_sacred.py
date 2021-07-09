34f17457a613b2d9b0082afd13cc4edebd015774
Mikkel Sørensen <m_hooge@hotmail.com>
2021-07-09 15:28:47 +0200

    Life is pain.

--- misc/fix_sacred.py
+++ misc/fix_sacred.py
@@ -12,24 +12,17 @@ def main(args):
             "eval_matched": "test.matched.accuracy",
             "eval_mismatched": "test.mismatched.accuracy"
         }
-        keys = [
-            "params", "nonzero_params",
-            "model_disk_size",
-            "theoretical_size", "eval_acc",
-            "eval_f1", "eval_matched", "eval_mismatched"
-        ]
-        for key in keys:
-            if getattr(args, key) is not None:
-                value = getattr(args, key)
-                sacred_key = acc_metrics_map.get(key, key)
-                print(f"Setting {sacred_key} to {value}")
-                time_now = datetime.now()
-                metrics_entry = {
-                    "steps": [0],
-                    "timestamp": [time_now.strftime("%Y-%m-%dT%H:%M%S.%f")],
-                    "values": [value]
-                }
-                metrics_data[sacred_key] = metrics_entry
+        for key in args.__dict__:
+            value = getattr(args, key)
+            sacred_key = acc_metrics_map.get(key, key)
+            print(f"Setting {sacred_key} to {value}")
+            time_now = datetime.now()
+            metrics_entry = {
+                "steps": [0],
+                "timestamp": [time_now.strftime("%Y-%m-%dT%H:%M%S.%f")],
+                "values": [value]
+            }
+            metrics_data[sacred_key] = metrics_entry
 
     with open(metrics_path, "w", encoding="utf-8") as fp:
         json.dump(metrics_data, fp, indent=2)
@@ -38,7 +31,7 @@ if __name__ == "__main__":
     AP = argparse.ArgumentParser()
 
     AP.add_argument("--name", type=str, required=True)
-    AP.add_argument("--params", type=int)
+    AP.add_argument("--model-params", type=int)
     AP.add_argument("--nonzero-params", type=int)
     AP.add_argument("--model-disk-size", type=float)
     AP.add_argument("--theoretical-size", type=float)
